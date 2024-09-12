a3f87a4594f6f14ce5763c91da75ae1e568d89b9
Wojciech Dlugosz <webmaster@styronet.pl>
2024-09-12 09:55:48 +0200

    Prepare single table page view

--- src/components/pages/SingleTable/SingleTable.js
+++ src/components/pages/SingleTable/SingleTable.js
@@ -1,6 +1,40 @@
+import { Form, Row, Col, InputGroup, Button } from "react-bootstrap";
+
 const SingleTable = () => {
     return (
-        <h1>SingleTable</h1>
+        <div className="mx-auto" style={{ width: '30%'}}>
+            <h1 className="pb-2">Table 1</h1>
+            <Row className="my-2">
+                <Col><b>Status: </b></Col>
+                <Col>
+                    <Form.Select>
+                        <option value="1">busy</option>
+                    </Form.Select>
+                </Col>
+            </Row>
+            <Row className="my-2">
+                <Col><b>People: </b></Col>
+                <Col xs="auto">
+                    <InputGroup><Form.Control style={{ maxWidth: '40px', marginRight: '-30px' }}></Form.Control></InputGroup>
+                </Col>
+                <Col xs="auto">
+                    <InputGroup.Text className="border-0 bg-transparent" >/</InputGroup.Text>
+                </Col>
+                <Col xs="auto">
+                    <InputGroup><Form.Control style={{ maxWidth: '40px', marginLeft: '-30px' }}></Form.Control></InputGroup>
+                </Col>
+            </Row>
+            <Row className="my-2">
+                <Col><b>Bill: </b></Col>
+                <Col xs="auto">
+                    <InputGroup.Text className="border-0 bg-transparent" style={{ marginRight: '-30px' }}>$</InputGroup.Text>
+                </Col>
+                <Col xs="auto">
+                    <InputGroup><Form.Control style={{ maxWidth: '40px' }}></Form.Control></InputGroup>
+                </Col>
+            </Row>
+            <Button>Update</Button>
+        </div>
     );
 };
 
