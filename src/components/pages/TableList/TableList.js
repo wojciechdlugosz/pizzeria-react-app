64fd47703803dec4f02a4b2b4711248c43a43f93
Wojciech Dlugosz <webmaster@styronet.pl>
2024-09-12 09:53:12 +0200

    Prepare all tables page view

--- src/components/pages/TableList/TableList.js
+++ src/components/pages/TableList/TableList.js
@@ -1,6 +1,15 @@
+import { ListGroup, Button } from "react-bootstrap";
+
 const TableList = () => {
     return (
-        <h1>TableList</h1>
+        <>
+        <h1>All tables</h1>
+        <ListGroup horizontal className="d-flex align-items-end border-bottom py-2">
+            <ListGroup.Item className="border-0"><h2>Table 1</h2></ListGroup.Item>
+            <ListGroup.Item className="border-0 py-1"><p><b>Status: </b>busy</p></ListGroup.Item>
+            <ListGroup.Item className="ms-auto border-0"><Button>Show more</Button></ListGroup.Item>
+        </ListGroup>
+        </>
     );
 };
 
