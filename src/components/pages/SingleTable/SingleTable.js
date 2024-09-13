34f57bc7a8a8d8fddb77f2040182d5bc97b90065
Wojciech Dlugosz <webmaster@styronet.pl>
2024-09-13 11:02:06 +0200

    Combine form values with local state

--- src/components/pages/SingleTable/SingleTable.js
+++ src/components/pages/SingleTable/SingleTable.js
@@ -2,35 +2,41 @@ import { Form, Row, Col, InputGroup, Button } from "react-bootstrap";
 import { useParams, Navigate } from "react-router-dom";
 import { getTableById } from "../../../redux/tablesRedux";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 const SingleTable = () => {
 
     const { tableId } = useParams();
     const tableData = useSelector((state) => getTableById(state, tableId));
 
+    const [status, setStatus] = useState(tableData.status || '');
+    const [peopleAmount, setPeopleAmount] = useState(tableData.peopleAmount || '0');
+    const [maxPeopleAmount, setMaxPeopleAmount] = useState(tableData.maxPeopleAmount || '');
+    const [bill, setBill] = useState(tableData.bill || '');
+
     if(!tableData) return <Navigate to="/" />
 
     return (
-        <div className="mx-auto" style={{ width: '30%'}}>
+        <Form className="mx-auto" style={{ width: '30%'}}>
             <h1 className="pb-2">Table {tableData.id}</h1>
             <Row className="my-2">
                 <Col><b>Status: </b></Col>
                 <Col>
                     <Form.Select>
-                        <option value="1">{tableData.status}</option>
+                        <option value={status}>{status}</option>
                     </Form.Select>
                 </Col>
             </Row>
             <Row className="my-2">
                 <Col><b>People: </b></Col>
                 <Col xs="auto">
-                    <InputGroup><Form.Control style={{ maxWidth: '40px', marginRight: '-30px' }} value={tableData.peopleAmount}></Form.Control></InputGroup>
+                    <InputGroup><Form.Control style={{ maxWidth: '40px', marginRight: '-30px' }} value={peopleAmount}></Form.Control></InputGroup>
                 </Col>
                 <Col xs="auto">
                     <InputGroup.Text className="border-0 bg-transparent" >/</InputGroup.Text>
                 </Col>
                 <Col xs="auto">
-                    <InputGroup><Form.Control style={{ maxWidth: '40px', marginLeft: '-30px' }} value={tableData.maxPeopleAmount}></Form.Control></InputGroup>
+                    <InputGroup><Form.Control style={{ maxWidth: '40px', marginLeft: '-30px' }} value={maxPeopleAmount}></Form.Control></InputGroup>
                 </Col>
             </Row>
             {tableData.status === 'Busy' ? (
@@ -40,12 +46,12 @@ const SingleTable = () => {
                         <InputGroup.Text className="border-0 bg-transparent" style={{ marginRight: '-30px' }}>$</InputGroup.Text>
                     </Col>
                     <Col xs="auto">
-                        <InputGroup><Form.Control style={{ maxWidth: '60px' }} value={tableData.bill}></Form.Control></InputGroup>
+                        <InputGroup><Form.Control style={{ maxWidth: '60px' }} value={bill}></Form.Control></InputGroup>
                     </Col>
                 </Row>
             ) : '' }
             <Button>Update</Button>
-        </div>
+        </Form>
     );
 };
 
